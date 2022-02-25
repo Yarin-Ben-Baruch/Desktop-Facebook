4138cb5ea808479724c89ae9758c17ee612a7f3a
Matan Korenfeld <m4t4n04@gmail.com>
2022-02-25 12:46:11 +0200

    fixing the sorting in likedphotos

--- Project1/LogicManager.cs
+++ Project1/LogicManager.cs
@@ -1,29 +1,28 @@
-﻿using System;
+﻿using FacebookWrapper.ObjectModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FacebookWrapper.ObjectModel;
 
-namespace FaceBookAppLogic
+namespace Project1
 {
-    public class LogicManager
+    class LogicManager
     {
         public List<Photo> FetchPhotos(FacebookObjectCollection<Album> i_Albums)
         {
             List<Photo> photos = new List<Photo>();
             List<Photo> sortedPhotos;
-            List<int> numberoflike = new List<int>();
+
             foreach (Album album in i_Albums)
             {
                 foreach (Photo photo in album.Photos)
                 {
                     photos.Add(photo);
-                    numberoflike.Add(photo.LikedBy.Count);
                 }
             }
 
-            sortedPhotos = photos.OrderBy(photo => -photo.LikedBy.Count).ToList();
+            sortedPhotos = photos.OrderBy(photo => photo.LikedBy.Count).ToList();
 
             return sortedPhotos;
         }
