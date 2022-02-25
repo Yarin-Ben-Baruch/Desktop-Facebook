ce80c08379937fe714a798fd854a173630976f0c
Yarin Ben Baruch <yarin1234565555@gmail.com>
2022-02-25 11:38:51 +0200

    Update logic

--- FacebookWinFormsApp/FormMain.cs
+++ FacebookWinFormsApp/FormMain.cs
@@ -8,6 +8,8 @@ namespace BasicFacebookFeatures
     public partial class FormMain : Form
     {
         private const string k_AppId = "507070034420577";
+        private ManagerLogic r_ManagerLogic;
+
 
         public User LoggedInUser { get; set; }
 
@@ -17,6 +19,92 @@ namespace BasicFacebookFeatures
         {
             InitializeComponent();
             FacebookService.s_CollectionLimit = 100;
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            loginAndInit();
+        }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        { 
+            FacebookService.LogoutWithUI();
+            resetWhenLogout();
+		}
+
+        private void m_ButtonBestMatch_Click(object sender, EventArgs e)
+        {
+            if (LoggedInUser != null)
+            {
+                new FormBestMatch(this).ShowDialog();
+            }
+            // TODO: ERROR MESSAGE
+        }
+
+        private void m_ButtonMostLikesFromFriends_Click(object sender, EventArgs e)
+        {
+            if (LoggedInUser != null)
+            {
+                new FormMostLikesFromFriends(this).ShowDialog();
+            }
+            // TODO: ERROR MESSAGE
+        }
+
+        private void m_ButtonPopularPhotos_Click(object sender, EventArgs e)
+        {
+            if (LoggedInUser != null)
+            {
+                //fetchPhotos();
+            }
+            // TODO : ERROR MESSAGE
+        }
+
+        private void m_ListBoxPopularPhotos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (m_ListBoxPopularPhotos.SelectedItems.Count == 1)
+            {
+                Photo selectedPage = m_ListBoxPopularPhotos.SelectedItem as Photo;
+                m_PictureBoxSelectedPopularPhoto.LoadAsync(selectedPage.PictureNormalURL);
+            }
+        }
+
+        private void fetchPhotos()
+        {
+            m_ListBoxPopularPhotos.Items.Clear();
+            m_ListBoxPopularPhotos.DisplayMember = "Name";
+
+            foreach (Album album in LoggedInUser.Albums)
+            {
+                foreach (Photo photo in album.Photos)
+                {
+                    m_ListBoxPopularPhotos.Items.Add(photo);
+                }
+            }
+
+            if (m_ListBoxPopularPhotos.Items.Count == 0)
+            {
+                MessageBox.Show("No Photo to retrieve :(");
+            }
+        }
+
+        private void resetWhenLogout()
+        {
+            LoggedInUser = null;
+            LoginResult = null;
+            m_ButtonLogin.Text = "Login";
+
+            m_PictureBoxProfilePhoto.Image = null;
+            m_LabelFullName.Text = "Full name";
+            m_LabelBirthday.Text = "Birthday";
+
+            m_ListBoxPopularPhotos.Items.Clear();
+        }
+
+        private void fetchUserInfo()
+        {
+            m_PictureBoxProfilePhoto.LoadAsync(LoggedInUser.PictureLargeURL);
+            m_LabelFullName.Text = LoginResult.LoggedInUser.Name;
+            m_LabelBirthday.Text = LoginResult.LoggedInUser.Birthday;
         }
 
         private void loginAndInit()
@@ -48,93 +136,6 @@ namespace BasicFacebookFeatures
             else
             {
                 MessageBox.Show(LoginResult.ErrorMessage, "Login Failed");
-            }
-        }
-
-        private void buttonLogin_Click(object sender, EventArgs e)
-        {
-            loginAndInit();
-        }
-
-        private void buttonLogout_Click(object sender, EventArgs e)
-        { 
-            FacebookService.LogoutWithUI();
-            resetWhenLogout();
-		}
-
-        private void resetWhenLogout()
-        {
-            LoggedInUser = null;
-            LoginResult = null;
-            m_ButtonLogin.Text = "Login";
-
-            m_PictureBoxProfilePhoto.Image = null;
-            m_LabelFullName.Text = "Full name";
-            m_LabelBirthday.Text = "Birthday";
-
-            m_ListBoxPopularPhotos.Items.Clear();
-
-        }
-
-        private void m_ButtonBestMatch_Click(object sender, EventArgs e)
-        {
-            if (LoggedInUser != null)
-            {
-                new FormBestMatch(this).ShowDialog();
-            }
-            // TODO: ERROR MESSAGE
-        }
-
-        private void m_ButtonMostLikesFromFriends_Click(object sender, EventArgs e)
-        {
-            if (LoggedInUser != null)
-            {
-                new FormMostLikesFromFriends(this).ShowDialog();
-            }
-            // TODO: ERROR MESSAGE
-        }
-
-        private void fetchUserInfo()
-        {
-            m_PictureBoxProfilePhoto.LoadAsync(LoggedInUser.PictureLargeURL);
-            m_LabelFullName.Text = LoginResult.LoggedInUser.Name;
-            m_LabelBirthday.Text = LoginResult.LoggedInUser.Birthday;
-        }
-
-        private void m_ButtonPopularPhotos_Click(object sender, EventArgs e)
-        {
-            if (LoggedInUser != null)
-            {
-                fetchPhotos();
-            }
-            // TODO : ERROR MESSAGE
-        }
-
-        private void m_ListBoxPopularPhotos_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if (m_ListBoxPopularPhotos.SelectedItems.Count == 1)
-            {
-                Photo selectedPage = m_ListBoxPopularPhotos.SelectedItem as Photo;
-                m_PictureBoxSelectedPopularPhoto.LoadAsync(selectedPage.PictureNormalURL);
-            }
-        }
-
-        private void fetchPhotos()
-        {
-            m_ListBoxPopularPhotos.Items.Clear();
-            m_ListBoxPopularPhotos.DisplayMember = "Name";
-
-            foreach (Album album in LoggedInUser.Albums)
-            {
-                foreach (Photo photo in album.Photos)
-                {
-                    m_ListBoxPopularPhotos.Items.Add(photo);
-                }
-            }
-
-            if (m_ListBoxPopularPhotos.Items.Count == 0)
-            {
-                MessageBox.Show("No Photo to retrieve :(");
             }
         }
     }
--- LogicFacbookApp/ManagerLogic.cs
+++ LogicFacbookApp/ManagerLogic.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+
 
 namespace LogicFacebookApp
 {
-    class ManagerLogic
+    public class ManagerLogic
     {
-        
+        public List<Photo> FetchPhotos(FacebookObjectCollection<Album> i_Albums)
+        {
+            List<Photo> photos = new List<Photo>();
+            List<Photo> sortedPhotos;
 
+            foreach (Album album in i_Albums)
+            {
+                foreach (Photo photo in album.Photos)
+                {
+                    photos.Add(photo);
+                }
+            }
+
+            sortedPhotos = photos.OrderBy(photo => photo.LikedBy.Count).ToList();
+
+            return sortedPhotos;
+        }
     }
 }
